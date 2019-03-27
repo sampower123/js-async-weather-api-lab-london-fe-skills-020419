625dc3070c8d54d63758e1e9d82b7f69bf741eeb
Sam Power <sam.g.power@gmail.com>
2019-03-27 20:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,93 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "12e9db9a1965c8c0e9d87c09be5c1fb2"
 
 function handleFormSubmit(event) {
   //handle submit event
+  document.addEventListener('submit',
+  function(event) {
+    event.preventDefault();
+    var formValue = document.getElementById("city").value
+    fetchCurrentWeather(formValue)
+  })
 }
 
-function fetchCurrentWeather(city) {
+function fetchCurrentWeather(formValue) {
   //fetch current weather based on city
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${formValue}&APPID=${API_KEY}`)
+  .then(resp => resp.json())
+  .then(json => displayCurrentWeather(json));
 }
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
+  
+  // current temp
+  var temp = json.main.temp
+  console.log(temp)
+  const current = document.querySelector('#current')
+  const currentTemp = document.createElement('currentTemp')
+  currentTemp.innerHTML = `<h2>${temp}°</h2>`
+  current.appendChild(currentTemp)
+  
+  // min temp
+  var min = json.main.temp_min
+  console.log(min)
+  const minId = document.querySelector('#min')
+  const minTemp = document.createElement('minTemp')
+  minTemp.innerHTML = `<h2>${min}°</h2>`
+  minId.appendChild(minTemp)
+  
+  // max temp
+  var max = json.main.temp_max
+  console.log(max)
+  const maxId = document.querySelector('#max')
+  const maxTemp = document.createElement('maxTemp')
+  maxTemp.innerHTML = `<h2>${max}°</h2>`
+  maxId.appendChild(maxTemp)
+  
+   // humidity
+  var humidity = json.main.humidity
+  console.log(max)
+  const humidityId = document.querySelector('#humidity')
+  const humidityTemp = document.createElement('humidityTemp')
+  humidityTemp.innerHTML = `<h2>${humidity}%</h2>`
+  humidityId.appendChild(humidityTemp)
+  
+   // cloud cover
+  var clouds = json.clouds.all
+  console.log(clouds)
+  const cloudsId = document.querySelector('#clouds')
+  const cloudsTemp = document.createElement('cloudsTemp')
+  cloudsTemp.innerHTML = `<h2>${clouds}%</h2>`
+  cloudsId.appendChild(cloudsTemp)
+  
 }
 
 
-function fetchFiveDayForecast(city) {
+function fetchFiveDayForecast(formValue) {
   //fetch five day forecast data based on city
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${formValue}&APPID=${API_KEY}`)
+  .then(resp => resp.json())
+  .then(json => displayCurrentWeather(json));
 }
 
 function displayFiveDayForecast(json) {
   //render five day forecast data to the DOM using provided IDs and json from API
+  
+  // forecast loop
+  const aside = document.querySelector('aside')
+  var forecast = json.list.main.temp
+  json.forEach(forecast => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${forecast}</h2>`
+    main.appendChild(h2)
+  })
 }
 
 function createChart(json) {
   //Bonus: render temperature chart using five day forecast data and ChartJS
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(event) {
   //add event listener here for form submission
+  handleFormSubmit(event);
 })
